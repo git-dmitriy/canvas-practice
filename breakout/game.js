7b45e8f337130adc57ad0ad3c5f4d0c58a1014d1
Dmitriy <shalberkind@mail.ru>
2020-10-07 22:25:00 +0300

    Рефакторинг - создание методов для обозначения структуры

--- breakout/game.js
+++ breakout/game.js
@@ -1,15 +1,50 @@
 // * План работы:
 
 // +todo Задача №1 - вывести фон
+// +todo Рефакторинг:
+//  выделение чёткой структуры
 
 "use strict";
+
 const game = {
-  start: function () {
+  ctx: null,
+  sprites: {
+    background: null,
+    ball: null,
+    platform: null,
+  },
+  init: function () {
     this.ctx = document.querySelector("#mycanvas").getContext("2d");
-    const background = new Image();
-    background.src = "img/background.png";
+  },
+  preload: function (callback) {
+    let loaded = 0;
+    const required = Object.keys(this.sprites).length;
+
+    for (let key in this.sprites) {
+      this.sprites[key] = new Image();
+      this.sprites[key].src = `img/${key}.png`;
+      this.sprites[key].addEventListener("load", () => {
+        loaded++;
+        if (loaded >= required) {
+          callback();
+        }
+      });
+    }
+  },
+  run: function () {
     window.requestAnimationFrame(() => {
-      this.ctx.drawImage(background, 0, 0);
+      this.render();
+    });
+  },
+  render: function () {
+    this.ctx.drawImage(this.sprites.background, 0, 0);
+    this.ctx.drawImage(this.sprites.ball, 0, 0);
+    this.ctx.drawImage(this.sprites.platform, 0, 0);
+  },
+  start: function () {
+    this.init();
+    this.preload(() => {
+      this.run();
     });
   },
 };
