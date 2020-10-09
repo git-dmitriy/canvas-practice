e1739e90991eeb55d51820348f9b4ec713fd315c
Dmitriy <shalberkind@mail.ru>
2020-10-09 23:50:30 +0300

    Создание объектов для платформы и мяча

--- breakout/game.js
+++ breakout/game.js
@@ -3,11 +3,14 @@
 // +todo Задача №1 - вывести фон
 // +todo Рефакторинг:
 //  выделение чёткой структуры
+// todo Расставить объекты по полю
 
 "use strict";
 
 const game = {
   ctx: null,
+  platform: null,
+  ball: null,
   sprites: {
     background: null,
     ball: null,
@@ -38,8 +41,18 @@ const game = {
   },
   render: function () {
     this.ctx.drawImage(this.sprites.background, 0, 0);
-    this.ctx.drawImage(this.sprites.ball, 0, 0);
-    this.ctx.drawImage(this.sprites.platform, 0, 0);
+    this.ctx.drawImage(
+      this.sprites.ball,
+      0,
+      0,
+      this.ball.width,
+      this.ball.height,
+      this.ball.x,
+      this.ball.y,
+      this.ball.width,
+      this.ball.height
+    );
+    this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
   },
   start: function () {
     this.init();
@@ -47,6 +60,18 @@ const game = {
       this.run();
     });
   },
+};
+
+game.ball = {
+  x: 320,
+  y: 280,
+  height: 20,
+  width: 20,
+};
+
+game.platform = {
+  x: 280,
+  y: 300,
 };
 
 window.addEventListener("load", () => {
