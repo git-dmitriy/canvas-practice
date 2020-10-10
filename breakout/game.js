45a8e4a44f9296c469e07d069926626fd4e387ce
Dmitriy <shalberkind@mail.ru>
2020-10-10 10:59:58 +0300

    Вывод блоков на игровое поле

--- breakout/game.js
+++ breakout/game.js
@@ -3,7 +3,7 @@
 // +todo Задача №1 - вывести фон
 // +todo Рефакторинг:
 //  выделение чёткой структуры
-// todo Расставить объекты по полю
+// +todo Расставить объекты по полю
 
 "use strict";
 
@@ -11,32 +11,58 @@ const game = {
   ctx: null,
   platform: null,
   ball: null,
+  blocks: [],
+  rows: 4,
+  cols: 8,
   sprites: {
     background: null,
     ball: null,
     platform: null,
+    block: null,
   },
   init: function () {
     this.ctx = document.querySelector("#mycanvas").getContext("2d");
+    console.log(this.ctx);
+    console.log("init is done");
   },
   preload: function (callback) {
     let loaded = 0;
     const required = Object.keys(this.sprites).length;
+    // let onImageLoad = () => {
+    //   ++loaded;
+    //   if (loaded >= required) {
+    //     console.log("preload is done");
+    //     callback();
+    //   }
+    // };
 
     for (let key in this.sprites) {
       this.sprites[key] = new Image();
       this.sprites[key].src = `img/${key}.png`;
+      console.log(this.sprites[key].src);
       this.sprites[key].addEventListener("load", () => {
         loaded++;
         if (loaded >= required) {
+          console.log("preload is done");
           callback();
         }
       });
     }
   },
+  create() {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++) {
+        this.blocks.push({
+          x: 64 * col + 65,
+          y: 24 * row + 35,
+        });
+      }
+    }
+  },
   run: function () {
     window.requestAnimationFrame(() => {
       this.render();
+      console.log("run is done");
     });
   },
   render: function () {
@@ -53,10 +79,17 @@ const game = {
       this.ball.height
     );
     this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+
+    for (let block of this.blocks) {
+      this.ctx.drawImage(this.sprites.block, block.x, block.y);
+    }
+
+    console.log("rended is done");
   },
   start: function () {
     this.init();
     this.preload(() => {
+      this.create();
       this.run();
     });
   },
