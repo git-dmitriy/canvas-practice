5e0038230908a1ec882cbefa2fab6fd8f1de2200
Dmitriy <shalberkind@mail.ru>
2020-10-29 00:25:15 +0300

    Новая версия сборщика

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 const { watch, parallel } = require("gulp");
-
 const browserSync = require("browser-sync").create();
 
-function server(done) {
+const project = {
+  breakOut: "breakout/",
+  appleHunter: "apple-hunter/",
+};
+
+function serverBreakOut(done) {
   browserSync.init({
     server: {
-      baseDir: `./breakout/`,
+      baseDir: project.breakOut,
       serveStaticOptions: {
         extensions: ["html"],
       },
@@ -14,12 +18,34 @@ function server(done) {
   done();
 }
 
-function watching(done) {
-  watch("./breakout/**/*.*", function reloading(done) {
+function watchingBreakOut(done) {
+  watch(`${project.breakOut}**/*.*`, (done) => {
+    browserSync.reload();
+    done();
+  });
+  done();
+}
+function serverAppleHunter(done) {
+  browserSync.init({
+    server: {
+      baseDir: project.appleHunter,
+      serveStaticOptions: {
+        extensions: ["html"],
+      },
+    },
+  });
+  done();
+}
+
+function watchingAppleHunter(done) {
+  watch(`${project.appleHunter}**/*.*`, (done) => {
     browserSync.reload();
     done();
   });
   done();
 }
 
-exports.default = parallel(server, watching);
+exports.breakOut = parallel(serverBreakOut, watchingBreakOut);
+exports.appleHunter = parallel(serverAppleHunter, watchingAppleHunter);
+
+// exports.default = parallel(server, watching);
