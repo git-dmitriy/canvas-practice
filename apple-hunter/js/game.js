038bc0cc8623c8bfe87bd5313f1eb519f736d3d8
Dmitriy <shalberkind@mail.ru>
2020-11-07 23:53:04 +0300

    Адаптация размера игры по высоте

--- apple-hunter/js/game.js
+++ apple-hunter/js/game.js
@@ -2,8 +2,18 @@ let game = {
   canvas: null,
   ctx: null,
   board: null,
-  width: 640,
-  height: 360,
+  width: 0,
+  height: 0,
+  dimensions: {
+    max: {
+      width: 640,
+      height: 360,
+    },
+    min: {
+      width: 300,
+      height: 300,
+    },
+  },
   sprites: {
     background: null,
     cell: null,
@@ -18,6 +28,39 @@ let game = {
   init() {
     this.canvas = document.getElementById("mycanvas");
     this.ctx = this.canvas.getContext("2d");
+    this.initDimensions();
+  },
+  initDimensions() {
+    let data = {
+      maxWidth: this.dimensions.max.width,
+      maxHeight: this.dimensions.max.height,
+      minWidth: this.dimensions.min.width,
+      minHeight: this.dimensions.min.height,
+      realWidth: window.innerWidth,
+      realHeight: window.innerHeight,
+    };
+
+    this.fitHeight(data);
+
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+  },
+  fitHeight(data) {
+    // realWidth / realHeight
+    // resultWidth / maxHeight
+    this.width = Math.floor(
+      (data.realWidth * data.maxHeight) / data.realHeight
+    );
+
+    // realWidth / realHeight
+
+    this.width = Math.min(this.width, data.maxWidth);
+    this.width = Math.max(this.width, data.minWidth);
+
+    // this.height = data.maxHeight;
+    this.height = Math.floor((this.width * data.realHeight) / data.realWidth);
+    this.canvas.style.height = "100%";
+    console.log(this.width, this.height);
   },
   preload(callback) {
     let loaded = 0;
@@ -41,7 +84,11 @@ let game = {
     this.snake.create();
 
     window.requestAnimationFrame(() => {
-      this.ctx.drawImage(this.sprites.background, 0, 0);
+      this.ctx.drawImage(
+        this.sprites.background,
+        (this.width - this.sprites.background.width) / 2,
+        (this.height - this.sprites.background.height) / 2
+      );
       this.board.render();
       this.snake.render();
     });
