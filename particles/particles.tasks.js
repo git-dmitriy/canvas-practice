d21a25e412cb09a394eefedcba4d0d21f5581d2a
Dmitriy <shalberkind@mail.ru>
2020-12-22 23:27:44 +0300

    Add namespace

--- particles/particles.tasks.js
+++ particles/particles.tasks.js
@@ -1,46 +1,58 @@
-const { watch, parallel, src, dest } = require("gulp");
+const { watch, parallel, series, src, dest } = require("gulp");
 const browserSync = require("browser-sync").create();
 const babel = require("gulp-babel");
+// const clean = require("gulp-clean");
 
-// const project = "particles";
+const project = "particles";
 
-// const path = {
-//   src: {
-//     html: `${project}/src/index.html`,
-//     css: `${project}/src/styles/main.css`,
-//     js: `${project}/src/js/index.js`,
-//   },
-//   build: {
-//     html: `${project}/build/index.html`,
-//     css: `${project}/build/styles/main.css`,
-//     js: `${project}/build/js/index.js`,
-//   },
-//   watch: {
-//     html: `${project}/src/**/*.html`,
-//     css: `${project}/src/styles/**/*.css`,
-//     js: `${project}/src/js/**/.js`,
-//   },
+const path = {
+  src: {
+    html: `${project}/src/**/*.html`,
+    css: `${project}/src/styles/main.css`,
+    js: `${project}/src/js/index.js`,
+  },
+  build: {
+    html: `${project}/build`,
+    css: `${project}/build/styles/main.css`,
+    js: `${project}/build/js/index.js`,
+  },
+  watch: {
+    html: `${project}/src/**/*.html`,
+    css: `${project}/src/styles/**/*.css`,
+    js: `${project}/src/js/**/.js`,
+  },
+  clean: `${project}/build/`,
+};
+
+function reload(cb) {
+  bs.reload();
+  cb();
+}
+
+// module.exports.cleanBuild = cleanBuild = () => {
+//   return src(path.clean, { read: false }).pipe(clean());
 // };
 
 module.exports.buildJs = function buildJS() {
-  return src("particles/src/js/index.js")
+  return src(path.src.js)
     .pipe(
       babel({
         presets: ["@babel/preset-env"],
       })
     )
-    .pipe(dest("particles/build"));
+    .pipe(dest(path.build.js));
 };
 
 module.exports.html = function html() {
-  return src("particles/src/index.html").pipe(dest("particles/build"));
+  return src(path.src.html).pipe(dest(path.build.html));
 };
+
 module.exports.css = function css() {
-  return src("particles/src/styles/main.css").pipe(dest("particles/build"));
+  return src(path.src.css).pipe(dest(path.build.css));
 };
 
 module.exports.watching = function watching(done) {
-  watch("particles/src/**/*.*", (done) => {
+  watch([path.src.html, path.src.css, path.src.js], (done) => {
     browserSync.reload();
     done();
   });
